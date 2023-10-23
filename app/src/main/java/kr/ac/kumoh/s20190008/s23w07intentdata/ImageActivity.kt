4684002b6af25406797ad1f3f304e40c6aa19a6e
EIVE <45841115+CHTuna@users.noreply.github.com>
2023-10-23 14:50:15 +0900

    try

--- app/src/main/java/kr/ac/kumoh/s20190008/s23w07intentdata/ImageActivity.kt
+++ app/src/main/java/kr/ac/kumoh/s20190008/s23w07intentdata/ImageActivity.kt
@@ -7,17 +7,23 @@ import kr.ac.kumoh.s20190008.s23w07intentdata.databinding.ActivityImageBinding
 import kr.ac.kumoh.s20190008.s23w07intentdata.databinding.ActivityMainBinding
 
 class ImageActivity : AppCompatActivity() {
+    companion object {
+        const val IMAGE_RESULT = "image result"
+        const val LIKE = 100
+        const val DISLIKE = 101
+        const val NONE = 0
+    }
     private lateinit var main: ActivityImageBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         main = ActivityImageBinding.inflate(layoutInflater)
         setContentView(main.root)
 
-        Log.i("Image!!!", intent.getStringExtra("location") ?: "없음")
+        Log.i("Image!!!", intent.getStringExtra(MainActivity.KEY_NAME) ?: "없음")
 
-        val res = when (intent.getStringExtra("location")){
-            "mountain" -> R.drawable.mountain
-            "sea" -> R.drawable.sea
+        val res = when (intent.getStringExtra(MainActivity.KEY_NAME)){
+            MainActivity.IMAGE_MOUNTAIN -> R.drawable.mountain
+            MainActivity.IMAGE_SEA -> R.drawable.sea
             else -> R.drawable.ic_launcher_foreground
         }
         main.image.setImageResource(res)
--- app/src/main/java/kr/ac/kumoh/s20190008/s23w07intentdata/MainActivity.kt
+++ app/src/main/java/kr/ac/kumoh/s20190008/s23w07intentdata/MainActivity.kt
@@ -1,34 +1,63 @@
 package kr.ac.kumoh.s20190008.s23w07intentdata
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.view.View.OnClickListener
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import kr.ac.kumoh.s20190008.s23w07intentdata.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnClickListener {
     companion object {
         const val KEY_NAME = "location"
+        const val IMAGE_MOUNTAIN = "mountain"
+        const val IMAGE_SEA = "sea"
     }
 
     private lateinit var main: ActivityMainBinding
+    private val startForResult = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) {
+        if (it.resultCode != Activity.RESULT_OK)
+            return@registerForActivityResult
+
+        val result = it.data?.getIntExtra(ImageActivity.IMAGE_RESULT, ImageActivity.NONE)
+        val str = when (result) {
+            ImageActivity.LIKE -> "좋아요"
+            ImageActivity.DISLIKE -> "싫어요"
+            else -> "에러"
+        }
+
+        Log.i("Result!!!", str)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         main = ActivityMainBinding.inflate(layoutInflater)
         setContentView(main.root)
 
-        main.btnMountain.setOnClickListener{
-            Toast.makeText(this, "산 이미지", Toast.LENGTH_SHORT).show()
+        main.btnMountain.setOnClickListener(this)
+        main.btnSea.setOnClickListener(this)
+    }
 
-            intent.putExtra(KEY_NAME, "mountain")
-            startActivity(intent)
+    override fun onClick(v: View?) {
+        val value = when (v?.id) {
+            main.btnMountain.id -> {
+                Toast.makeText(this, "산 이미지", Toast.LENGTH_SHORT).show()
+                IMAGE_MOUNTAIN
+            }
+            main.btnSea.id -> {
+                Toast.makeText(this, "바다 이미지", Toast.LENGTH_SHORT).show()
+                IMAGE_SEA
+            }
+            else -> return
         }
-        main.btnSea.setOnClickListener{
-            Toast.makeText(this, "바다 이미지", Toast.LENGTH_SHORT).show()
-
-            val intent = Intent(this, ImageActivity::class.java)
-            intent.putExtra(KEY_NAME, "sea")
-            startActivity(intent)
-        }
+        val intent = Intent(this, ImageActivity::class.java)
+        intent.putExtra(KEY_NAME, value)
+        // startActivity(intent)
+        startForResult.launch(intent)
     }
 }
